883efdb66f202589e3edb515acc9ed6878b3053b
Adam <adamparker@lclark.edu>
2017-01-25 22:18:05 -0800

    first commit: main banner is up. working on container blocks and rest of website

---
+++ tmnt2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel= "stylesheet" type= "text/css" href= "movie.css"/>
+</head>
+<body>
+	<div id= "banner">
+		<img class= "center" src="https://webster.cs.washington.edu/images/rancidbanner.png" alt= "this" >
+    </div>
+	<h1 class= "title"><?php
+	echo "i love pupperz";
+	?></h1>
+	<div id= "content-area">
+		<img class= "rottenbanner" src= "https://webster.cs.washington.edu/images/rottenlarge.png" alt= "ROTTEN" border-radius= "20px">
+		33%
+	</div>
+	
+</body>
+</html>
